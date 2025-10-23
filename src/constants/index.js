7440ce10d804380a3ce69e3a1123582a177232dc
NourMohamedDerbeli <derbelnourmohamed@gmail.com>
2025-10-23 17:37:18 +0100

    Add link to social media

--- src/constants/index.js
+++ src/constants/index.js
@@ -72,21 +72,10 @@ const techStackIcons = [
 
 const socialImgs = [
   {
-    name: "insta",
-    imgPath: "/images/insta.png",
-  },
-  {
-    name: "fb",
-    imgPath: "/images/fb.png",
-  },
-  {
-    name: "x",
-    imgPath: "/images/x.png",
-  },
-  {
     name: "linkedin",
     imgPath: "/images/linkedin.png",
-  },
+    url: "https://www.linkedin.com/in/md-nr-db/"
+  }
 ];
 
 export {
